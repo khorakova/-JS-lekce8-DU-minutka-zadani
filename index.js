90cd513cdebc6f15aaa71db81bdbb52382ee0835
khorakova <horakoak@gmail.com>
2023-03-14 21:49:06 +0100

    reseni

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 console.log('funguju!');
+
+let time = prompt("Zadej za kolik vteřin má budík zazvonit.")
+let timeInMiliSecond = time * 1000
+
+function ring () {
+    let alarmClock = document.querySelector(".alarm")
+    alarmClock.classList.add("alarm--ring")
+    let audio = document.querySelector(".audio")
+    audio.play()
+}
+
+const timerID = setTimeout(ring, timeInMiliSecond)
+
+let canceledButton = document.querySelector(".canceled");
+canceledButton.addEventListener('click', stopRing)
+
+function stopRing () {
+    clearTimeout(timerID)
+}
+
+
